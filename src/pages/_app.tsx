d398a06aca00b57b107d33cc8e6a4e03eaa52719
Marlon-Dener <marlon.dener@voltbras.com.br>
2021-12-29 15:45:58 -0300

    Add miragejs integration

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,16 +1,28 @@
+import { QueryClient, QueryClientProvider } from 'react-query';
+import { ReactQueryDevtools } from 'react-query/devtools';
+
 import { ChakraProvider } from '@chakra-ui/react';
 import { AppProps } from 'next/app';
 
 import { SidebarDrawerProvider } from '../contexts/SidebarDrawerContext';
-import { theme } from '../styles/theme';
+// eslint-disable-next-line import-helpers/order-imports
+import { makeServer } from '../services/mirage';
 
+if (process.env.NODE_ENV === 'development') {
+  makeServer();
+}
+import { theme } from '../styles/theme';
+const queryClient = new QueryClient();
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <ChakraProvider resetCSS theme={theme}>
-      <SidebarDrawerProvider>
-        <Component {...pageProps} />
-      </SidebarDrawerProvider>
-    </ChakraProvider>
+    <QueryClientProvider client={queryClient}>
+      <ChakraProvider resetCSS theme={theme}>
+        <SidebarDrawerProvider>
+          <Component {...pageProps} />
+        </SidebarDrawerProvider>
+      </ChakraProvider>
+      <ReactQueryDevtools />
+    </QueryClientProvider>
   );
 }
 
--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -1,4 +1,6 @@
+import { useEffect } from 'react';
 import { RiAddLine, RiPencilLine } from 'react-icons/ri';
+import { useQuery } from 'react-query';
 
 import {
   Box,
@@ -15,18 +17,22 @@ import {
   Tbody,
   Text,
   useBreakpointValue,
+  Spinner,
 } from '@chakra-ui/react';
 import Link from 'next/link';
 
 import { Header } from '../../components/Header';
 import { Pagination } from '../../components/Pagination';
 import { Sidebar } from '../../components/Sidebar';
+import { useUsers } from '../../services/hooks/useUsers';
 
 export default function UserList() {
+  const { data, isLoading, error, isFetching } = useUsers();
   const isWideVersion = useBreakpointValue({
     base: false,
     lg: true,
   });
+
   return (
     <Box>
       <Header />
@@ -37,6 +43,9 @@ export default function UserList() {
           <Flex mb="8" justify="space-between" align="center">
             <Heading size="lg" fontWeight="normal">
               Usuários
+              {!isLoading && isFetching && (
+                <Spinner size="sm" color="gray.500" ml="4" />
+              )}
             </Heading>
 
             <Link href="/users/create" passHref>
@@ -52,107 +61,69 @@ export default function UserList() {
             </Link>
           </Flex>
 
-          <Table colorScheme="whiteAlpha">
-            <Thead>
-              <Tr>
-                <Th px={['4', '4', '6']} color="gray.300" width="8">
-                  <Checkbox colorScheme="pink" />
-                </Th>
-                <Th>Usuários</Th>
-                {isWideVersion && <Th>Data de cadastro</Th>}
-                <Th></Th>
-              </Tr>
-            </Thead>
-            <Tbody>
-              <Tr>
-                <Td px={['4', '4', '6']}>
-                  <Checkbox colorScheme="pink" />
-                </Td>
+          {isLoading ? (
+            <Flex justify="center">
+              <Spinner />
+            </Flex>
+          ) : error ? (
+            <Flex justify="center">
+              <Text>Falha ao obter dados</Text>
+            </Flex>
+          ) : (
+            <>
+              <Table colorScheme="whiteAlpha">
+                <Thead>
+                  <Tr>
+                    <Th px={['4', '4', '6']} color="gray.300" width="8">
+                      <Checkbox colorScheme="pink" />
+                    </Th>
+                    <Th>Usuários</Th>
+                    {isWideVersion && <Th>Data de cadastro</Th>}
+                    <Th></Th>
+                  </Tr>
+                </Thead>
+                <Tbody>
+                  {data?.map((user) => {
+                    return (
+                      <Tr key={user.id}>
+                        <Td px={['4', '4', '6']}>
+                          <Checkbox colorScheme="pink" />
+                        </Td>
 
-                <Td>
-                  <Box>
-                    <Text fontWeight="bold">Marlon Dener</Text>
-                    <Text fontSize="sm" color="gray.300">
-                      marlon.dener@voltbras.com.br
-                    </Text>
-                  </Box>
-                </Td>
-                {isWideVersion && <Td px={['4', '4', '6']}>11/05/2021</Td>}
-                {isWideVersion && (
-                  <Td>
-                    <Button
-                      as="a"
-                      size="sm"
-                      fontSize="small"
-                      colorScheme="none"
-                      borderWidth={1}
-                      borderColor="gray.100"
-                      leftIcon={<Icon as={RiPencilLine} />}
-                    >
-                      {isWideVersion ? 'Editar' : ''}
-                    </Button>
-                  </Td>
-                )}
-              </Tr>
-              <Tr>
-                <Td px={['4', '4', '6']}>
-                  <Checkbox colorScheme="pink" />
-                </Td>
-
-                <Td>
-                  <Box>
-                    <Text fontWeight="bold">Marlon Dener</Text>
-                    <Text fontSize="sm" color="gray.300">
-                      marlon.dener@voltbras.com.br
-                    </Text>
-                  </Box>
-                </Td>
-                {isWideVersion && <Td px={['4', '4', '6']}>11/05/2021</Td>}
-                {isWideVersion && (
-                  <Td>
-                    <Button
-                      as="a"
-                      size="sm"
-                      fontSize="small"
-                      colorScheme="none"
-                      borderWidth={1}
-                      borderColor="gray.100"
-                      leftIcon={<Icon as={RiPencilLine} />}
-                    />
-                  </Td>
-                )}
-              </Tr>
-              <Tr>
-                <Td px={['4', '4', '6']}>
-                  <Checkbox colorScheme="pink" />
-                </Td>
-
-                <Td>
-                  <Box>
-                    <Text fontWeight="bold">Marlon Dener</Text>
-                    <Text fontSize="sm" color="gray.300">
-                      marlon.dener@voltbras.com.br
-                    </Text>
-                  </Box>
-                </Td>
-                {isWideVersion && <Td px={['4', '4', '6']}>11/05/2021</Td>}
-                {isWideVersion && (
-                  <Td>
-                    <Button
-                      as="a"
-                      size="sm"
-                      fontSize="small"
-                      colorScheme="none"
-                      borderWidth={1}
-                      borderColor="gray.100"
-                      leftIcon={<Icon as={RiPencilLine} />}
-                    />
-                  </Td>
-                )}
-              </Tr>
-            </Tbody>
-          </Table>
-          <Pagination />
+                        <Td>
+                          <Box>
+                            <Text fontWeight="bold">{user.name}</Text>
+                            <Text fontSize="sm" color="gray.300">
+                              {user.email}
+                            </Text>
+                          </Box>
+                        </Td>
+                        {isWideVersion && (
+                          <Td px={['4', '4', '6']}>{user.createdAt}</Td>
+                        )}
+                        {isWideVersion && (
+                          <Td>
+                            <Button
+                              as="a"
+                              size="sm"
+                              fontSize="small"
+                              colorScheme="none"
+                              borderWidth={1}
+                              borderColor="gray.100"
+                              leftIcon={<Icon as={RiPencilLine} />}
+                            >
+                              Editar
+                            </Button>
+                          </Td>
+                        )}
+                      </Tr>
+                    );
+                  })}
+                </Tbody>
+              </Table>
+              <Pagination />
+            </>
+          )}
         </Box>
       </Flex>
     </Box>
